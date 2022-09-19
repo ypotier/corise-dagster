b96407a36cd18ebafe1f5d1ba094d8181d4830e2
Yohann Potier <ypotier@tesseratx.com>
2022-09-19 10:05:22 -0400

    add redis and process data

--- week_4/project/week_4.py
+++ week_4/project/week_4.py
@@ -21,16 +21,47 @@ def get_s3_data(context)-> List[Stock]:
     return output
 
 
-@asset
-def process_data():
-    # Use your op logic from week 3 (you will need to make a slight change)
-    pass
+@asset(
+    group_name="corise",
+    op_tags={"kind": "bi"},
+    description="Find the highest stock price and date",
+)
+def process_data(get_s3_data: List[Stock]) -> Aggregation:
+    most_expensive_stock = max(get_s3_data, key=lambda stock: stock.high)
+    return Aggregation(date=most_expensive_stock.date, high=most_expensive_stock.high)
 
 
-@asset
-def put_redis_data():
-    # Use your op logic from week 3 (you will need to make a slight change)
-    pass
+@asset(
+    group_name="corise",
+    required_resource_keys={"redis"},
+    op_tags={"kind": "redis"},
+    description="Post aggregate result to Redis",
+)
+def put_redis_data(context, process_data: Aggregation) -> None:
+    context.log.debug(f"Putting {process_data} to Redis")
+    context.resources.redis.put_data(
+        name=f"{process_data.date}",
+        value=process_data.high,
+    )
 
 
-get_s3_data_docker, process_data_docker, put_redis_data_docker = with_resources()
+get_s3_data_docker, process_data_docker, put_redis_data_docker = with_resources(
+    definitions=[get_s3_data, process_data, put_redis_data],
+    resource_defs={"s3": s3_resource, "redis": redis_resource},
+    resource_config_by_key={
+        "s3": {
+            "config": {
+                "bucket": "dagster",
+                "access_key": "test",
+                "secret_key": "test",
+                "endpoint_url": "http://localstack:4566",
+            }
+        },
+        "redis": {
+            "config": {
+                "host": "redis",
+                "port": 6379
+            },
+        },
+    }
+)
